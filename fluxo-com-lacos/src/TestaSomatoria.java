b0060b2d45497a2397f78ce001525f377cfc86fd
Lia Santos <liasilva77@acad.charqueadas.ifsul.edu.br>
2021-04-18 19:11:12 -0300

    enviando exercício aula 07 - escopo nos laços :repeat:

--- fluxo-com-lacos/src/TestaSomatoria.java
+++ fluxo-com-lacos/src/TestaSomatoria.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class TestaSomatoria {
+
+public class TestaSomatoria {
+    public static void main(String[] args) {
+
+        int contador = 0;
+        int total = 0; //variáveis declaradas fora do laço
+
+        while(contador <= 10) {
+            //total = total + contador;
+            total += contador;
+            contador++;
+            System.out.println(total);
+        }
+        System.out.println("O total é " + total);
+    }
+
 }
