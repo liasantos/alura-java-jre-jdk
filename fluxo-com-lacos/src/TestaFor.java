4f33ba19269efff4c9d75cbf8a94a29722b70ad6
Lia Santos <liasilva77@acad.charqueadas.ifsul.edu.br>
2021-04-18 19:20:52 -0300

    exercicio aula 07 - laço com for :repeat:

---
+++ fluxo-com-lacos/src/TestaFor.java
@@ -0,0 +1,12 @@
+
+public class TestaFor {
+
+    public static void main(String[] args){
+
+        for( int contador = 0; contador <= 10 ; contador++ ){ //var só existe dentro do escopo
+            System.out.println(contador);
+
+        }
+    }
+
+}
