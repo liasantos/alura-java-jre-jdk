ceebb721fd27b3b7f531642a40bdf7c7c3e549ba
Lia Santos <liasilva77@acad.charqueadas.ifsul.edu.br>
2021-04-18 18:36:40 -0300

    Enviando exercicio de revisão :pencil:

--- sintaxe-variaveis-e-fluxo/src/ExercicioRevisao.java
+++ sintaxe-variaveis-e-fluxo/src/ExercicioRevisao.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class ExercicioRevisao {
+
+/*
+João gostaria de criar um programa sobre Imposto de Renda (IR) e verificou as regras de alíquota.
+Ele descobriu no site da receita:
+De 1900.0 até 2800.0, o IR é de 7.5% e pode deduzir na declaração o valor de R$ 142
+De 2800.01 até 3751.0, o IR é de 15% e pode deduzir R$ 350
+De 3751.01 até 4664.00, o IR é de 22.5% e pode deduzir R$ 636*/
+
+public class ExercicioRevisao {
+
+    public static void main(String[] args) {
+
+        double salario = 3300.0;
+
+        if(salario >= 1900.0 && salario <= 2800.0){
+
+            System.out.println("O IR é de 7.5%. Você pode deduzir o valor de R$ 142.");
+
+        } else {
+
+            if(salario >= 2800.01 && salario <= 3751.0) {
+
+                System.out.println("O IR é de 15%. Você pode deduzir o valor de R$ 350.");
+
+            } else {
+
+                if(salario >= 3751.01 && salario <= 4664.00) {
+
+                    System.out.println("O IR é de 22.5%. Você pode deduzir até R$ 636.");
+
+                }
+            }
+        }
+    }
 }
